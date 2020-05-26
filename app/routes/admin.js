be6f817513590fe5f62f6410ff150384e68d3317
Esequias Figueredo <esequiasfigueredo@gmail.com>
2020-05-25 23:22:12 -0300

    teste

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,7 +5,7 @@ module.exports = function(application){
     });
     application.post('/noticias/salvar', function(req,res){
         application.app.controllers.admin.noticias_salvar(application,req,res);
-    });
+    });// testste
 
         
 }
